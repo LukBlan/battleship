02d9e89f0b5acc0c6032c5c03cb79a3ce5fb7704
lukblan <luk.blanko@gmail.com>
2023-12-08 15:23:14 -0300

    Add html and MiniCss webpack plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -5,6 +7,18 @@ module.exports = {
   entry: '/src/example.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: '[name].[contenthash].js',
+    filename: '[name]-[contenthash].js',
+    clean: true
   },
+
+  module: {
+    rules: [
+      {test: /\.css$/i,use: [MiniCssExtractPlugin.loader, "css-loader"],}
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/template/index.html'}),
+    new MiniCssExtractPlugin({filename : "[name]-[hash].css"})
+  ],
 };
