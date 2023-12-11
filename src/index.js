7dac10c282090831b770f517eb8eb74725055e25
lukblan <luk.blanko@gmail.com>
2023-12-11 20:52:26 -0300

    Initialize dependencies to start the game

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles.css';
+import { GameFactory } from './domain/game/game-factory';
+import { GameController } from './domain/game/game-controller';
+import { GameBoardFactory } from './domain/board/game-board-factory';
 
-console.log('test test');
+const players = [
+  { name: 'Player1', size: 10 },
+  { name: 'Player2', size: 10 },
+];
+
+const gameBoardFactory = new GameBoardFactory();
+const gameFactory = new GameFactory();
+const game = gameFactory.create(players, gameBoardFactory);
+const gameController = new GameController(game);
+gameController.init();
