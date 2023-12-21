0685211084ffebedc81f9953d17d70b6498bce8a
lukblan <luk.blanko@gmail.com>
2023-12-21 18:39:27 -0300

    Refactor webpage style
    Change element styles
    Add fonts
    Add build files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name]-[contenthash].js',
     clean: true,
+    assetModuleFilename: 'asset/[hash][ext][query]',
   },
 
   module: {
--- src/ui/components/game-configuration-screen/game-configuration-screen.js
+++ src/ui/components/game-configuration-screen/game-configuration-screen.js
@@ -1,30 +1,23 @@
 import './game-configuration-screen.css';
 
-function generateShipSection(ships) {
-  const container = document.createElement('div');
-
-  ships.forEach((ship) => {
-    container.append(ship);
-  });
-
-  return container;
-}
-
 function gameConfigurationScreen() {
   const screen = document.createElement('div');
   const board = document.createElement('div');
   const title = document.createElement('h2');
   const shipSection = document.createElement('div');
+  const footer = document.createElement('div');
 
   board.classList.add('board');
   shipSection.classList.add('ships-section');
   screen.classList.add('configuration-screen');
   title.innerText = 'Arrange your boats';
   title.classList.add('configuration-title');
+  footer.classList.add('footer');
 
   screen.append(title);
   screen.append(board);
   screen.append(shipSection);
+  screen.append(footer);
   return screen;
 }
 
