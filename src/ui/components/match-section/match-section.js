c60b8586938d6e2379589ea860cfa0f07bd979fd
lukblan <luk.blanko@gmail.com>
2023-12-23 15:12:39 -0300

    Add new style to match section

--- src/ui/components/match-section/match-section.js
+++ src/ui/components/match-section/match-section.js
@@ -1,14 +1,26 @@
 import './match-section.css';
 
+function createPlayerSection(titleText, className) {
+  const playerSection = document.createElement('div');
+  const title = document.createElement('h3');
+  const playerBoard = document.createElement('div');
+
+  playerSection.classList.add('player-section');
+  title.classList.add('player-title');
+  title.innerText = titleText;
+  playerBoard.classList.add(className);
+  playerSection.append(title);
+  playerSection.append(playerBoard);
+
+  return playerSection;
+}
+
 function matchSection() {
   const container = document.createElement('div');
-  const playerBoard = document.createElement('div');
-  const computerBoard = document.createElement('div');
+  const playerBoard = createPlayerSection('Player Board', 'player-board');
+  const computerBoard = createPlayerSection('Computer Board', 'computer-board');
 
   container.classList.add('match-section');
-  playerBoard.classList.add('player-board');
-  computerBoard.classList.add('computer-board');
-
   container.append(playerBoard);
   container.append(computerBoard);
   return container;
