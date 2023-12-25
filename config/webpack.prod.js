d2a09cbf9c2ef0a13c4dc809f27a3deeae0797a1
lukblan <luk.blanko@gmail.com>
2023-12-25 17:40:53 -0300

    Add css minimizer and compiled files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -18,8 +19,20 @@ module.exports = {
     ],
   },
 
+  optimization: {
+    minimize: true,
+  },
+
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/template/index.html' }),
+    new HtmlWebpackPlugin({
+      template: './src/template/index.html',
+      minify: {
+        removeComments: true,
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+      },
+    }),
     new MiniCssExtractPlugin({ filename: '[name]-[contenthash].css' }),
+    new CssMinimizerPlugin(),
   ],
 };
